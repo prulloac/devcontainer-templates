27c5a22413fce7184fdf1dbe387cb2d8be59d974
Pablo Ulloa <pablo.ulloac@usach.cl>
2024-03-26 04:03:20 +0000

    updated template

--- test/java_typescript/test.sh
+++ test/java_typescript/test.sh
@@ -4,7 +4,15 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "color" [ $(cat /tmp/color.txt | grep red) ]
+check "java" java --version
+check "bun" bun --version
+check "node" node --version
+check "npm" npm --version
+check "yarn" yarn --version
+check "docker" docker --version
+check "docker-compose" docker-compose --version
+check "git" git --version
+check "gh" gh --version
 
 # Report result
 reportResults
